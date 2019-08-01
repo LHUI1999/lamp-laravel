18ce62c6fd1b2432a56e37083c982edecb765295
LHUI1999 <1753517736@qq.com>
2019-08-01 17:27:34 +0800

    laravel lihui

---
+++ app/Http/Controllers/Home/IndexController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+
+class IndexController extends Controller
+{
+    //前台首页
+    public function index()
+    {
+    	$cates = DB::table('cates')->get();
+    	$data = [];
+    	foreach($cates as $k => $v){
+    		// echo '0,'.$k;
+    		if($v->path == '0'){
+    			$data[$v->cname] = $v->cname;
+    			// $data[$k]['path'] = $v->path;
+    			// $data[$k][]=[1,2,3];
+    		}
+    		
+
+    	}
+    	
+    	dd($data);
+
+
+    	
+
+    	
+    	return view('home.index.index',['cates_1'=>$cates]);
+    }
+
+
+}
